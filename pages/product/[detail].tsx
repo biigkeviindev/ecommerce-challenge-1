8d1a7af96b9588c198f8648ab8395737a5b033a1
Kevin Rivera <hi.kevindev@gmail.com>
2025-02-04 13:24:22 +0100

    feat: User can select capacity & color phone

--- pages/product/[detail].tsx
+++ pages/product/[detail].tsx
@@ -14,13 +14,14 @@ export default function ProductDetail() {
 
   console.log(productDetail);
   const [styleSelector, setStyleSelector] = useState(
-    productDetail.colorOptions[0]
+    productDetail?.colorOptions[0]
   );
-  const [capacitySelector, setCapacitySelector] = useState();
+  const [capacitySelector, setCapacitySelector] = useState(
+    productDetail?.storageOptions[0]
+  );
 
   if (error || !productDetail) return <p>No se encontro el telefono.</p>;
 
-  console.log(`Color: ${styleSelector.name}`);
   return (
     <Layout>
       <section className="detail-view">
@@ -28,7 +29,7 @@ export default function ProductDetail() {
           <div>
             <img
               className="detail-view__image-url"
-              src={styleSelector.imageUrl}
+              src={styleSelector?.imageUrl}
               alt=""
             />
           </div>
@@ -36,7 +37,7 @@ export default function ProductDetail() {
           <div>
             <h3 className="detail-view__product-name">{productDetail?.name}</h3>
             <p className="detail-view__product-price">
-              From {productDetail.basePrice} EUR
+              From {productDetail?.basePrice} EUR
             </p>
           </div>
 
@@ -44,8 +45,17 @@ export default function ProductDetail() {
             <section>
               <p>Storage ¿hOW MUCH SPACE DO YOU NEED?</p>
               <div className="detail-view__feature-box">
-                {productDetail.storageOptions.map((storage: any) => (
-                  <div className="detail-view__capacity-box">
+                {productDetail?.storageOptions.map((storage: any) => (
+                  <div
+                    onClick={() => setCapacitySelector(storage)}
+                    className="detail-view__capacity-box"
+                    style={{
+                      borderColor:
+                        storage?.capacity === capacitySelector?.capacity
+                          ? "black"
+                          : "",
+                    }}
+                  >
                     <p>{storage.capacity}</p>
                   </div>
                 ))}
@@ -55,18 +65,19 @@ export default function ProductDetail() {
             <section className="Color">
               <p>color. pick your favourite.</p>
               <div className="detail-view__feature-box">
-                {productDetail.colorOptions.map((storage: any) => (
+                {productDetail?.colorOptions.map((style: any) => (
                   <div
                     className="detail-view__color-box"
+                    onClick={() => setStyleSelector(style)}
                     style={{
-                      backgroundColor: storage.hexCode,
+                      backgroundColor: style.hexCode,
+                      borderColor:
+                        style?.hex === styleSelector?.hex ? "#000" : "",
                     }}
-                  >
-                    {" "}
-                  </div>
+                  />
                 ))}
               </div>
-              <p>{styleSelector.name}</p>
+              <p>{styleSelector?.name}</p>
             </section>
             <button className="detail-view__add-cart">AÑADIR</button>
           </div>
