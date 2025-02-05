264e8bf68a1d6e547689ddc8890a9547dce9df4d
biigkeviindev <biigkeviiin@gmail.com>
2025-02-05 12:05:36 +0100

    feat: Add pro development

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: !isProd
+  },
+  webpack(config) {
+    if (isProd) {
+      config.mode = 'production';
+      config.optimization.minimize = true
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
